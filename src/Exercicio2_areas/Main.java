d4a77c1898fcd81ee7339e4251c11e009dca2aae
kelly <kelly.kellory@gmail.com>
2022-05-24 05:14:14 -0300

    Áreas de figuras planas - Quadrado, Retângulo, Trapézio e Losango

---
+++ src/Exercicio2_areas/Main.java
@@ -0,0 +1,14 @@
+package Exercicio2_areas;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Exercicio de Cálculo das Áreas");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5 , 5);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5, 2);
+
+    }
+}
